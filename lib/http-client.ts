3ff07a64b2f76c8234edaf011a8ded1bc0e7c8c8
Borewit <borewit@users.noreply.github.com>
2020-01-05 18:20:28 +0100

    Simplify and clean-up code

--- lib/http-client.ts
+++ lib/http-client.ts
@@ -1,35 +1,13 @@
 import * as initDebug from 'debug';
 import * as _fetch from 'node-fetch';
-import { IContentRangeType, IHeadRequestInfo, IRangeRequestResponse } from '@tokenizer/range'; // Add 'fetch' API for node.js
-import { IHttpClient } from './types';
+import { IContentRangeType, IHeadRequestInfo, IRangeRequestResponse, IRangeRequestClient, parseContentRange } from '@tokenizer/range'; // Add 'fetch' API for node.js
 
 const debug = initDebug('streaming-http-token-reader:http-client');
-
-export function parseContentRange(contentRange: string): IContentRangeType {
-  debug(`_parseContentRang response: contentRange=${contentRange}`);
-
-  if (contentRange) {
-    const parsedContentRange = contentRange.match(
-      /bytes (\d+)-(\d+)\/(?:(\d+)|\*)/i
-    );
-    if (!parsedContentRange) {
-      throw new Error('FIXME: Unknown Content-Range syntax: ' + contentRange);
-    }
-
-    return {
-      firstBytePosition: parseInt(parsedContentRange[1], 10),
-      lastBytePosition: parseInt(parsedContentRange[2], 10),
-      instanceLength: parsedContentRange[3] ? parseInt(parsedContentRange[3], 10) : null
-    };
-  } else {
-    return null;
-  }
-}
 
 /**
  * Simple HTTP-client, which both works in node.js and browser
  */
-export class HttpClient implements IHttpClient {
+export class HttpClient implements IRangeRequestClient {
 
   private static getContentLength(headers: _fetch.Headers): number {
     const contentLength = headers.get('Content-Length');
@@ -57,14 +35,13 @@ export class HttpClient implements IHttpClient {
   constructor(private url: string) {
   }
 
-  public getHeadInfo(): Promise<IHeadRequestInfo> {
-    return _fetch(this.url, {method: 'HEAD'}).then(resp => {
-      this.resolvedUrl = resp.url;
-      return HttpClient.makeResponse(resp);
-    });
+  public async getHeadInfo(): Promise<IHeadRequestInfo> {
+    const response = await _fetch(this.url, {method: 'HEAD'});
+    this.resolvedUrl = response.url;
+    return HttpClient.makeResponse(response);
   }
 
-  public getResponse(method: string, range?: [number, number]): Promise<IRangeRequestResponse> {
+  public async getResponse(method: string, range?: [number, number]): Promise<IRangeRequestResponse> {
     if (range) {
       debug(`_getResponse ${method} ${range[0]}..${range[1]}`);
     } else {
@@ -74,14 +51,13 @@ export class HttpClient implements IHttpClient {
     const headers = new _fetch.Headers();
     headers.set('Range', 'bytes=' + range[0] + '-' + range[1]);
 
-    return _fetch(this.resolvedUrl || this.url, {method, headers}).then(response => {
-      if (response.ok) {
-        this.resolvedUrl = response.url;
-        return HttpClient.makeResponse(response);
-      } else {
-        throw new Error(`Unexpected HTTP response status=${response.status}`);
-      }
-    });
+    const response = await _fetch(this.resolvedUrl || this.url, {method, headers});
+    if (response.ok) {
+      this.resolvedUrl = response.url;
+      return HttpClient.makeResponse(response);
+    } else {
+      throw new Error(`Unexpected HTTP response status=${response.status}`);
+    }
   }
 
 }
